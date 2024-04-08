6d724485020eea120dce0ab736beeb0b717b5bb1
Zavier Hopson <zo.hopson94@gmail.com>
2024-04-08 10:23:22 -0500

    incorrect message and ex handling added

--- Program.cs
+++ Program.cs
@@ -6,8 +6,42 @@ create variable to store user input
 receive input from user (ReadLine)
 display guess*/
 
+/*Phase 2:
+create 'secret number' variable, hard-coded
+removed display of user guess
+if user guess is correct, display message. if not, display different message */
+
+
+int secretNumber = 45;
+bool userGuessedCorrect = false;
+
 Console.WriteLine(@"Welcome to The Guessing Game!
 Please enter a number: ");
-int numberGuessed = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"You've guessed {numberGuessed}!");
+while (userGuessedCorrect == false)
+{
+    try
+    {
+        int numberGuessed = int.Parse(Console.ReadLine());
+        if (numberGuessed == secretNumber)
+        {
+            Console.WriteLine("MATHEMATICAL! You guessed the secret number!");
+            userGuessedCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Um, no. Not it.");
+        }
+
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("That was not a number. Please enter a number.");
+
+
+    }
+
+}
+
+
+
